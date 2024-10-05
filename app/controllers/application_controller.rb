62166e21632decdffef53238d27fc96173973915
EC2 Default User <ec2-user@ip-172-31-38-63.ap-northeast-1.compute.internal>
2024-10-05 15:33:35 +0000

    ログインと一覧まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,keys:[:email]) # 注目
     end
+  protected
+
+  def after_sign_in_path_for(resource)
+    books_path
+  end    
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,17 +2,21 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
+def after_sign_in_path_for(resource)
+    books_path
+end
 
   # 投稿データの保存
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    if @book.save
-      redirect_to book_path(@book), notice: '書籍が正常に作成されました。'
-    else
-      render :new
-    end
+def create
+  @book = Book.new(book_params)
+  @book.user_id = current_user.id
+  if @book.save
+    redirect_to books_path, notice: '書籍が正常に作成されました。'
+  else
+    render :new
   end
+end
 
   def index
     @books = Book.all
@@ -22,7 +26,6 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  # 投稿データのストロングパラメータ
   private
 
   def book_params
