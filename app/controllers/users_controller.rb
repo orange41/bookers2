9047dd2b5755c264de18f55fba93369b87efc314
EC2 Default User <ec2-user@ip-172-31-38-63.ap-northeast-1.compute.internal>
2024-10-14 15:31:25 +0000

    修正中2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,74 +1,80 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, only: [:edit, :update]
+before_action :authenticate_user, only: [:edit, :update]
 
-  def new
-    @user = User.new
-  end
+before_action :authenticate_user!, except: [:index, :show]
+end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = "successfully Login"
-      redirect_to login_path
-    else
-      flash[:error] = "Error in registration"
-      render 'new'
-    end
-  end
+class BooksController < ApplicationController
+before_action :authenticate_user!
+end
 
-  def edit
-    @user = current_user
-  end
+def new
+@user = User.new
+end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      flash[:success] = "successfully update"
-      redirect_to user_path(@user)
-    else
-      flash[:error] = "Error in updating profile"
-      render 'edit'
-    end
-  end
+def create
+@user = User.new(user_params)
+if @user.save
+flash[:success] = "successfully Login"
+redirect_to login_path
+else
+flash[:error] = "Error in registration"
+render 'new'
+end
+end
 
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    flash[:success] = "successfully destroy"
-    redirect_to root_path
-  end
+def edit
+@user = current_user
+end
 
-  def login
-      flash[:success] = "successfully Login"
-  end
+def update
+@user = User.find(params[:id])
+if @user.update(user_params)
+flash[:success] = "successfully update"
+redirect_to user_path(@user)
+else
+flash[:error] = "Error in updating profile"
+render 'edit'
+end
+end
 
-  def logout
-      flash[:success] = "successfully Logout"
-  end
-  
-  def index
-    @users = User.all
-  end
-  
+def destroy
+@user = User.find(params[:id])
+@user.destroy
+flash[:success] = "successfully destroy"
+redirect_to root_path
+end
 
-  private
+def login
+flash[:success] = "successfully Login"
+end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
-  end
+def logout
+flash[:success] = "successfully Logout"
+end
 
-  def authenticate_user
-    unless logged_in?
-      flash[:alert] = "Login is required"
-      redirect_to login_path
-    end
-  end
+def index
+@users = User.all
+end
 
-  def logged_in?
-    !!current_user
-  end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+private
+
+def user_params
+params.require(:user).permit(:name, :email, :password)
+end
+
+def authenticate_user
+unless logged_in?
+flash[:alert] = "Login is required"
+redirect_to login_path
+end
+end
+
+def logged_in?
+!!current_user
+end
+
+def current_user
+@current_user ||= User.find(session[:user_id]) if session[:user_id]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get "homes/about" => "homes#about", as: "about"
+  get '/home/about', to: 'homes#about'
   get '/users', to: 'users#index'
   resources :books, only: [:new, :create, :index, :show, :destroy, :update]
   resources :users, only: [:show, :edit, :update]
