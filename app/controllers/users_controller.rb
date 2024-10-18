b424c5bd6ecdf97b6569d21af8a04a4906eef0ce
EC2 Default User <ec2-user@ip-172-31-38-63.ap-northeast-1.compute.internal>
2024-10-18 17:35:30 +0000

    修正中11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,33 +11,32 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "User successfully created"
-      redirect_to user_path(@user) 
+      redirect_to user_path(@user)
     else
       flash[:error] = "Error in registration"
       render 'new'
     end
   end
 
-def show
-  @user = User.find(params[:id])
-  @books = @user.books.page(params[:page])
-end
-
-def edit
-  @user = User.find(params[:id])  # ここで current_user を使わない
-
-end
-
-def update
-  @user = User.find(params[:id])
-  if @user.update(user_params)
-    flash[:success] = "User successfully updated"
-    redirect_to user_path(@user)
-  else
-    flash[:error] = "Error updating user"
-    render 'edit'
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books.page(params[:page])
   end
-end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "User successfully updated"
+      redirect_to user_path(@user)  # 編集完了後にユーザーページにリダイレクト
+    else
+      flash[:error] = "Error updating user"
+      render 'edit'
+    end
+  end
 
   def destroy
     @user = User.find(params[:id])
@@ -47,28 +46,28 @@ end
   end
 
   def login
-    @user = User.find(params[:id]) # ログインするユーザーを取得するコードを追加
+    @user = User.find(params[:id])
     session[:user_id] = @user.id
     flash[:success] = "Signed in successfully."
     redirect_to user_path(@user)
   end
 
   def logout
-    session[:user_id] = nil # セッションを削除するコードを追加
+    session[:user_id] = nil
     flash[:success] = "Successfully logged out"
     redirect_to root_path
   end
 
-def index
-  @users = User.all
-  @user = current_user
-end
+  def index
+    @users = User.all
+    @user = current_user
+  end
 
   private
 
-def user_params
-  params.require(:user).permit(:name, :profile_image, :introduction, :email)  # 必要なパラメータを追加
-end
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction, :email)
+  end
 
   def is_matching_login_user
     @user = User.find(params[:id])
